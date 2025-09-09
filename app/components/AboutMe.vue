7f77656b7967b300d4b2e7a5fd0d1ba373fd2f40
Jowohny <vujohny109238@gmail.com>
2025-09-08 23:03:40 -0700

    Update AboutMe.vue

--- app/components/AboutMe.vue
+++ app/components/AboutMe.vue
@@ -3,13 +3,31 @@
 </script>
 
 <template>
-    <div class="min-h-screen flex items-center justify-center">
-        <div class="h-[65vh] px-8 text-left">
-            <h1 class="text-white text-3xl font-inter tracking-wide font-thin ">
-                As introduced, my name is Johny Vu. <br> 
+    <div class="min-h-screen flex flex-col items-center">
+        <div>
+            <h1 class="block h-[15vh] text-center text-7xl content-center bg-clip-text text-transparent bg-linear-to-r/decreasing from-indigo-500 to-teal-400 font-inter tracking-wider font-thin">
+                About Me
             </h1>
         </div>
-        <img src="/images/JohnyVu.jpg" class="border rounded-3xl h-[65vh] w-auto object-contain">
+        <div class="flex flex-row w-3/5">
+            <div class="h-[65vh] px-8 text-left">
+                <h1 class="text-white text-2xl font-inter block text-center tracking-wide font-thin mb-2 px-8">
+                    As introduced, my name is Johny Vu.
+                </h1>
+                <p class="text-white opacity-80 text-xl font-inter text-left tracking-wide font-thin leading-snug">
+                    I am many things, but for the purpose of this portfolio, I'll keep it short... I am a Frontend Developer
+                    who specializes in optimized and fun animations primarily through the use of GSAP (Green Sock Animation
+                    Platform). <br><br>
+                    I also try my best to avoid vibe coding when it comes to creating these sites to make the best out of my 
+                    own creativity. What good is a Frontend Developer without a good sense of how to organize things on their
+                    own? <br><br>
+                    Since this is not only something to put myself out there, but also a passion project I put some time and
+                    effort into, the non-important stuff like my hobbies and separate side projects will be off in another 
+                    section to keep the site from getting flooded.
+                </p>
+            </div>
+            <img src="/images/JohnyVu.jpg" class="border rounded-3xl h-[65vh] w-auto object-contain">
+        </div>
     </div>
 
 </template>
