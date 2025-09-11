b7f68d30eeb80d0b7df3c368b9a58b3a4024af34
Jowohny <vujohny109238@gmail.com>
2025-09-10 23:57:20 -0700

    About Me progress (cont.)

--- app/components/AboutMe.vue
+++ app/components/AboutMe.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import { gsap } from 'gsap'
 import { SplitText } from 'gsap/SplitText'
+import { ScrambleTextPlugin } from 'gsap/all'
+
+gsap.registerPlugin(ScrambleTextPlugin) 
 
 const sectionReferences = ref<HTMLDivElement[]>([])
 const sectionTweens = ref<gsap.core.Timeline[]>([])
+const nameplateRef = ref<HTMLHeadingElement>()
 let timeline: GSAPTimeline | null = null
+let mainline: GSAPTimeline | null = null
 
 const experienceTransition = () => {
     console.log('apoadsfijpeoijfpaoeijfpoaijefpoaijefpoaije')
@@ -66,8 +71,35 @@ const addSectionRef = (el: Element | ComponentPublicInstance | null) => {
 
 onMounted(() => {
     timeline = gsap.timeline()
+    mainline = gsap.timeline()
 
     gsap.set([...sectionReferences.value], { opacity: 0, y: 100, scaleX: 0 })
+    gsap.set('.fishsauce', { opacity: 0, x: -100, y: -100 })
+
+    mainline.to(nameplateRef.value!, {
+        scrambleText: {
+            text: 'As introduced, my name is Johny Vu',
+            chars: 'XO',
+            speed: 0.5
+        },
+        duration: 1.5
+    })
+    .to(nameplateRef.value!, {
+        scrambleText: {
+            text: 'Johny Vu',
+            chars: '1234675890',
+            speed: 0.2
+        },
+        duration: 2
+    })
+    .to('.fishsauce', {
+        duration: 1.5,
+        stagger: 0.5,
+        opacity: 1,
+        x: 0,
+        y:0,
+        ease: 'power4.inOut'
+    })
 
     timeline.to([...sectionReferences.value], {
         duration: 1,
@@ -95,20 +127,25 @@ const sectionAnimate = (index: number) => {
         </div>
         <div class="flex flex-row w-3/5 mb-8">
             <div class="h-[65vh] px-8 text-left mr-8">
-                <h1 class="text-white text-2xl font-inter block text-center tracking-wide font-thin mb-2 px-8">
-                    As introduced, my name is Johny Vu.
+                <h1 ref="nameplateRef" class="text-white text-2xl font-inter block text-center tracking-wide font-thin mb-2 px-8">
+                    
                 </h1>
-                <p class="text-white opacity-80 text-xl font-inter text-left tracking-wide font-thin leading-snug mb-8">
-                    I am many things, but for the purpose of this portfolio, I'll keep it short... I am a Frontend Developer
+                <p class="text-white opacity-80 text-xl font-inter text-left tracking-wide font-thin leading-snug mb-4 fishsauce">
+                    I am many things, but for the purpose of this portfolio, I'll keep it short... at least I'll try to. I am a Frontend Developer
                     who specializes in optimized and fun animations primarily through the use of GSAP (Green Sock Animation
-                    Platform). <br><br>
+                    Platform).
+                </p>
+                <p class="text-white opacity-80 text-xl font-inter text-left tracking-wide font-thin leading-snug mb-4 fishsauce">
                     I also try my best to avoid vibe coding when it comes to creating these sites to make the best out of my 
                     own creativity. What good is a Frontend Developer without a good sense of how to organize things on their
-                    own? <br><br>
+                    own?
+                </p>
+                <p class="text-white opacity-80 text-xl font-inter text-left tracking-wide font-thin leading-snug mb-8 fishsauce">
                     Since this is not only something to put myself out there, but also a passion project I put some time and
                     effort into, the non-important stuff like my hobbies and separate side projects will be off in another 
                     section to keep the site from getting flooded.
                 </p>
+
                 <img src="/images/AboutMe/GithubContributions.png" class="border rounded-xl border-none w-full">
             </div>
             <img src="/images/JohnyVu.jpg" class="border rounded-3xl h-[65vh] w-auto object-contain">
