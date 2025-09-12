a8bec57c8b21bebfd5ca5287569507a345f1f1cb
Jowohny <vujohny109238@gmail.com>
2025-09-11 23:52:00 -0700

    about me progress (cont.)

--- app/components/AboutMe.vue
+++ app/components/AboutMe.vue
@@ -7,9 +7,9 @@ gsap.registerPlugin(ScrambleTextPlugin)
 
 const sectionReferences = ref<HTMLDivElement[]>([])
 const sectionTweens = ref<gsap.core.Timeline[]>([])
-const nameplateRef = ref<HTMLHeadingElement>()
+const nameplateRef = ref<HTMLHeadingElement | null>(null)
+const selfImageRef = ref<HTMLImageElement | null>( null)
 let timeline: GSAPTimeline | null = null
-let mainline: GSAPTimeline | null = null
 
 const experienceTransition = () => {
     console.log('apoadsfijpeoijfpaoeijfpoaijefpoaijefpoaije')
@@ -71,37 +71,61 @@ const addSectionRef = (el: Element | ComponentPublicInstance | null) => {
 
 onMounted(() => {
     timeline = gsap.timeline()
-    mainline = gsap.timeline()
 
     gsap.set([...sectionReferences.value], { opacity: 0, y: 100, scaleX: 0 })
-    gsap.set('.fishsauce', { opacity: 0, x: -100, y: -100 })
+    gsap.set('.fishsauce', { opacity: 0, xPercent: 100 })
+    gsap.set(selfImageRef.value, { scale: 0 })
 
-    mainline.to(nameplateRef.value!, {
+    timeline.to(selfImageRef.value, {
+        scale: 1,
+        duration: 1,
+        ease: 'power4.inOut'
+    })
+    .to(selfImageRef.value, {
+        border: 5,
+        duration: 0.5,
+        ease: 'power2.out'
+    }, '<')
+    .to(selfImageRef.value, {
+        border: 0,
+        duration: 0.5,
+        ease: 'power1.inOut'
+    }, '<+=0.4')
+    .to(nameplateRef.value, {
         scrambleText: {
             text: 'As introduced, my name is Johny Vu',
             chars: 'XO',
-            speed: 0.5
+            speed: 0.2
         },
-        duration: 1.5
+        duration: 1
     })
-    .to(nameplateRef.value!, {
+    .to(nameplateRef.value, {
+        yPercent: -100,
+        opacity: 0,
+        duration: 1
+    })
+    .fromTo(nameplateRef.value, 
+    {
+        yPercent: 100,
+    },
+    {
         scrambleText: {
             text: 'Johny Vu',
             chars: '1234675890',
-            speed: 0.2
+            speed: 1
         },
-        duration: 2
+        duration: 2,
+        yPercent: 0,
+        opacity: 1
     })
     .to('.fishsauce', {
         duration: 1.5,
         stagger: 0.5,
         opacity: 1,
-        x: 0,
-        y:0,
+        xPercent: 0,
         ease: 'power4.inOut'
-    })
-
-    timeline.to([...sectionReferences.value], {
+    }, '<+=0.2')
+    .to([...sectionReferences.value], {
         duration: 1,
         ease: 'power4.inOut',
         y: 0,
@@ -121,15 +145,13 @@ const sectionAnimate = (index: number) => {
     
     <div class="min-h-screen flex flex-col items-center">
         <div>
-            <h1 class="block h-[15vh] text-center text-7xl content-center bg-clip-text text-transparent bg-linear-to-r/decreasing from-indigo-500 to-teal-400 font-inter tracking-wider font-thin">
+            <h1 class="block h-[15vh] text-center text-6xl content-center bg-clip-text text-transparent bg-linear-to-r/decreasing from-indigo-500 to-teal-400 font-inter tracking-wider font-thin">
                 About Me
             </h1>
         </div>
         <div class="flex flex-row w-3/5 mb-8">
-            <div class="h-[65vh] px-8 text-left mr-8">
-                <h1 ref="nameplateRef" class="text-white text-2xl font-inter block text-center tracking-wide font-thin mb-2 px-8">
-                    
-                </h1>
+            <div class="h-[65vh] flex flex-col px-8 text-left mr-8">
+                <h1 ref="nameplateRef" class="text-white text-2xl font-inter block text-center tracking-tight font-thin mb-2 px-8">( ͡° ͜ʖ ͡°)</h1>
                 <p class="text-white opacity-80 text-xl font-inter text-left tracking-wide font-thin leading-snug mb-4 fishsauce">
                     I am many things, but for the purpose of this portfolio, I'll keep it short... at least I'll try to. I am a Frontend Developer
                     who specializes in optimized and fun animations primarily through the use of GSAP (Green Sock Animation
@@ -146,9 +168,9 @@ const sectionAnimate = (index: number) => {
                     section to keep the site from getting flooded.
                 </p>
 
-                <img src="/images/AboutMe/GithubContributions.png" class="border rounded-xl border-none w-full">
+                <img src="/images/AboutMe/GithubContributions.png" class="border rounded-xl border-none w-full mt-auto">
             </div>
-            <img src="/images/JohnyVu.jpg" class="border rounded-3xl h-[65vh] w-auto object-contain">
+            <img ref="selfImageRef" src="/images/JohnyVu.jpg" class="border rounded-3xl h-[65vh] w-auto object-contain z-10">
         </div>
         <div class="flex flex-row border-t-1 pt-2 w-2/3 justify-center">
             <div 
